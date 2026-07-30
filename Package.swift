8c27ff91ea8d3ef2d7562bd170f04b6d75e0f479
Ahmed M. Elhalaby <ahmedm.elhalaby@gmail.com>
2026-07-30 18:41:23 +0300

    build: repin AinkradAppKit at its AinkradHome merge commit

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
-        .package(url: "https://github.com/AhmedMElhalaby/AinkradAppKit", revision: "6ad16ff9a651d12358d53533b1a0402b4c478975"),
+        .package(url: "https://github.com/AhmedMElhalaby/AinkradAppKit", revision: "e4882adb97af2892b55870b55c397e9ec9453def"),
     ],
     targets: [
         .executableTarget(
