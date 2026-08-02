bd75ec31d5430d7e1ce850c1cb1b6a4709aec0b2
Ahmed M. Elhalaby <ahmedm.elhalaby@gmail.com>
2026-08-02 16:27:13 +0300

    chore: pin AinkradAppKit to 60036ad, including the embedded template

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
-        .package(url: "https://github.com/AhmedMElhalaby/AinkradAppKit", revision: "31f89bc9e186354077035316a832237ce6993a09"),
+        .package(url: "https://github.com/AhmedMElhalaby/AinkradAppKit", revision: "60036ad9abe5d0ca4e84109bcc78a51f7b8578f0"),
     ],
     targets: [
         .executableTarget(
